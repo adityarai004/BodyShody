a42454b6e7c745d5cf052385f37168040ccfdc61
Aditya <adityaadiirai.004@gmail.com>
2023-04-28 18:42:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bodyshody/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.bodyshody
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var startLl:LinearLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startLl = findViewById(R.id.startLl)
+        startLl.setOnClickListener{
+            Toast.makeText(this,"Exercise will start soon!!",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
